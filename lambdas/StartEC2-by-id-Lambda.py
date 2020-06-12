85d33376f34c6ae8adc477d116679c75f716765f
Fabricio <fabgcruz@gmail.com>
2020-06-12 10:29:09 -0300

    Update lambda

---
+++ lambdas/StartEC2-by-id-Lambda.py
@@ -0,0 +1,9 @@
+import boto3
+
+region = 'sa-east-1'
+instances = ['i-0d01316cfcd385865']
+
+def lambda_handler(event, context):
+    
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.start_instances(InstanceIds=instances)
