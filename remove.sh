d1f6a2168f4893021e135c670a999638ac566c15
Fabricio Gonçalves <35773621+fabgcruz@users.noreply.github.com>
2022-03-26 01:56:48 -0300

    Delete remove.sh

--- remove.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while read F  ; do
-        /usr/local/bin/aws  --region="sa-east-1" cloudformation delete-stack  --stack-name $F 
-        echo "Deletando o arquivo ....." 
-        echo $F
-        done <stack-remover.txt
